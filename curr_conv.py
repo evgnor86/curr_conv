77044c78ecf783fb5bb001b2689496068b2cb2ab
evgnor86 <atxn@yandex.ru>
2021-11-10 18:23:29 +0700

    Final commit

--- curr_conv.py
+++ curr_conv.py
@@ -1,16 +1,4 @@
 import extensions
 
-
-# def start():
-#     try:
-#         # result = quote of base * amount
-#         result = extensions.CurrencyConvertor.get_price(base='USD', quote='RUB', amount='10')
-#     except extensions.APIException as e:
-#         print(f'{e}')
-#     else:
-#         print(result)
-
-
 if __name__ == '__main__':
-    # start()
     bot = extensions.TelegramBot()
